be620f53e827ec474aec0bf43494accb188bf077
toastloaf <144608860+toastloaf@users.noreply.github.com>
2023-11-26 14:40:00 +0100

    Initial Commit

--- Nettside/Scripts/FadeIn.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('#OrderButton').addEventListener('click', function() {
-    document.querySelector('#MainContent').classList.add('show');
-});
-
-document.querySelector('#InfoButton').addEventListener('click', function() {
-    document.querySelector('#MainContent').classList.add('show');
-});
